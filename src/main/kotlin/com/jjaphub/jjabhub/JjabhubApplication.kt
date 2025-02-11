4ae680b9a23f231dd625c145ae55c2e03e5dd5b3
SonJHo <wnsgh708222@naver.com>
2025-02-12 02:52:25 +0900

    초기설정

---
+++ src/main/kotlin/com/jjaphub/jjabhub/JjabhubApplication.kt
@@ -0,0 +1,11 @@
+package com.jjaphub.jjabhub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JjabhubApplication
+
+fun main(args: Array<String>) {
+	runApplication<JjabhubApplication>(*args)
+}
---
+++ src/test/kotlin/com/jjaphub/jjabhub/JjabhubApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jjaphub.jjabhub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JjabhubApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
